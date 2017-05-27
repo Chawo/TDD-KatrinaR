b4e7a752928c1f1ec9c6ece7499d71387c98e4a3
Katrina R <katrinarosales@live.com>
2017-05-27 10:46:55 +0200

    Exercise 1 - Email validator

---
+++ TDD-Excercise/ValidationEngine/emailAdress.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ValidationEngine
+{
+    public class emailAdress
+    {
+        private string email { get; set; }
+        private bool result { get; set; }
+
+        public string _email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+         
+        public emailAdress(string emailIn)
+        {
+            _email = emailIn;
+        }
+
+        public bool ValidAdress()
+        {
+            return Regex.IsMatch(email,
+              @"^(?("")(""[^""]+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))" +
+              @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
+        }
+    }
+}
---
+++ TDD-Excercise/ValidationEngineTests/EmailValidation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ValidationEngine;
+
+namespace ValidationEngineTests
+{
+    [TestFixture]
+    public class EmailValidation
+    {
+        [Test]
+        public void SendingInTrueValidation()
+        {
+            string EmailAdress = "mejl@live.com";
+
+            var sut = new emailAdress(EmailAdress);
+
+            var istrue = sut.ValidAdress();
+
+            Assert.IsTrue(istrue);
+        }
+
+        [Test]
+        public void SendingInFalseValidation()
+        {
+            string EmailAdress = "Test.com";
+
+            var sut = new emailAdress(EmailAdress);
+
+            var isfalse = sut.ValidAdress();
+
+            Assert.IsFalse(isfalse);
+        }
+
+        [Test]
+        public void SendingInNullValidation()
+        {
+            string EmailAdress = "";
+
+            var sut = new emailAdress(EmailAdress);
+
+            var isFalseIfNull = sut.ValidAdress();
+
+            Assert.IsFalse(isFalseIfNull);
+        }
+    }
+}
