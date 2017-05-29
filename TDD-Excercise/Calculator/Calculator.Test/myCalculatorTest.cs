ae05b1baafb5e557122ddf36fad1852fe49f2c12
Katrina R <katrinarosales@live.com>
2017-05-29 23:37:06 +0200

    Adding Calculator

---
+++ TDD-Excercise/Calculator/Calculator.Test/myCalculatorTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+using NUnit.Framework;
+
+namespace Calculator.Test
+{
+    public class myCalculatorTest
+    {
+        myCalculator sut = new myCalculator();
+
+        [Test]
+        [TestCase ("")]
+        public void ReturnZero(string addNumbers)
+        {
+            var number = sut.Add(addNumbers);
+            Assert.AreEqual(0, number);
+        }
+
+        [Test]
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        public void ReturnGivingNumber(int expected, string addNumbers)
+        {
+            var number = sut.Add(addNumbers);
+            Assert.AreEqual(expected, number);
+        }
+
+        [Test]
+        [TestCase(3, "1,2")]
+        [TestCase(6, "1,2,3")]
+        public void ReturnSumOfNumbers(int expected, string addNumbers)
+        {
+            var number = sut.Add(addNumbers);
+            Assert.AreEqual(expected, number);
+        }
+    }
+}
---
+++ TDD-Excercise/Calculator/Calculator/myCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class myCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            else if (!numbers.Contains(","))
+            {
+                return int.Parse(numbers);
+            }
+            else
+            {
+                var myNumbers = new List<int>();
+                // Skapa så att när det kommer fler nummer i stringen ska de summeras ihop
+                return 0;
+
+            }
+
+        }
+
+    }
+}
