da65a1a814c8c124177daca531cb31b6c5b54960
Katrina R <katrinarosales@live.com>
2017-05-29 23:28:29 +0200

    Calculator

---
+++ TDD-Excercise/TravelAgency/TourSchedule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency
+{
+    public class TourSchedule
+    {
+        private string Name { get; set; }
+        private DateTime DateOfTheTour { get; set; }
+        private int AvailableNumberOfSeats { get; set; }
+
+        //public TourSchedule(string _name, DateTime _dateOfTheTour, int _availableNumberOfSeats)
+        //{
+        //    Name = _name;
+        //    DateOfTheTour = _dateOfTheTour;
+        //    AvailableNumberOfSeats = _availableNumberOfSeats;
+        //}
+
+        private List<TourSchedule> TourCalendar = new List<TourSchedule>();
+        
+        public void CreateTour(string name, DateTime dateOfTheTour, int availableNumberOfSeats)
+        {
+            var newTour = new TourSchedule()
+            {
+                Name = name,
+                DateOfTheTour = dateOfTheTour,
+                AvailableNumberOfSeats = availableNumberOfSeats
+            };
+
+            TourCalendar.Add(newTour);
+
+        }
+
+        public List<TourSchedule> GetToursFor(DateTime inDateTime)
+        {
+            if (inDateTime.Date == null)
+            {
+
+            }
+            var result = TourCalendar.FindAll(x=>x.DateOfTheTour == inDateTime.Date);
+            return result;
+        }
+    }
+}
---
+++ TDD-Excercise/TravelAgencyTests/TourScheduleTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TravelAgency;
+
+namespace TravelAgencyTests
+{
+    [TestFixture]
+    public class TourScheduleTests
+    {
+        TourSchedule sut = new TourSchedule();
+
+        [Test]
+        public void CanCreateNewTour()
+        {
+
+            sut.CreateTour(
+            "New years day safari",
+            new DateTime(2013, 1, 1), 20);
+             
+        }
+
+        [Test]
+        public void ExpectOneTourFromList()
+        {
+                        
+            var dateTime = DateTime.Parse("2013, 1, 1");
+            var result = sut.GetToursFor(dateTime);
+             
+
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [Test]
+        public void ToursAreScheduledByDateOnly()
+        {
+            
+        }
+    }
+}
