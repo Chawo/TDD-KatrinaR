2c31a1c6e18179e18ba31633b95b34f7d7b02236
Katrina R <katrinarosales@live.com>
2017-05-30 22:43:39 +0200

    Adding Excercise 4

---
+++ TravelAgency-Excercise-4/TravelAgency/TourSchedule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency
+{
+    public class TourSchedule
+    {
+        public string Name { get; set; }
+        public DateTime DateOfTheTour { get; set; }
+        public int AvailableNumberOfSeats { get; set; }
+
+        private List<TourSchedule> TourCalendar = new List<TourSchedule>();
+
+        public void CreateTour(string name, DateTime dateOfTheTour, int availableNumberOfSeats)
+        {
+
+            var result = TourCalendar.Count(x => x.DateOfTheTour.Date == dateOfTheTour.Date);
+
+            if (result >= 3)
+            {
+                throw new TourAllocationException(dateOfTheTour.AddDays(1).Date);
+            }
+            else
+            {
+                var newTour = new TourSchedule()
+                {
+                    Name = name,
+                    DateOfTheTour = dateOfTheTour,
+                    AvailableNumberOfSeats = availableNumberOfSeats
+                };
+
+                TourCalendar.Add(newTour);
+            }
+
+
+        }
+
+        public List<TourSchedule> GetToursFor(DateTime inDateTime)
+        {
+            return TourCalendar.Where(x => x.DateOfTheTour.Date == inDateTime.Date).ToList();
+        }
+    }
+}
